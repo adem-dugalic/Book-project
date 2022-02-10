e2d0a7455c4f468892ccc69059b1793ad8cf8b8e
skeri123 <31672243+skeri123@users.noreply.github.com>
2022-02-10 13:47:51 +0100

    error handling and async for routes

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,68 +1,74 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get all books
 // @route GET /api/books
 // @access public
 
-const getBooks = (req, res) => {
+const getBooks = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Books" });
-};
+});
 
 // @desc Get a book
 // @route GET /api/books/:id
 // @access public
 
-const getBook = (req, res) => {
+const getBook = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get Book ${req.params.id}` });
-};
+});
 
 // @desc Get authors of a book
 // @route GET /api/books/:id/authors
 // @access public
 
-const getBookAuthors = (req, res) => {
+const getBookAuthors = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Book Authors` });
-};
+});
 
 // @desc Set books
 // @route POST /api/books
 // @access private
 
-const setBooks = (req, res) => {
+const setBooks = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(201).json({ message: "Set Books" });
-};
+});
 
 // @desc Set authors of a book
 // @route POST /api/books/:id/authors
 // @access private
 
-const setBookAuthors = (req, res) => {
+const setBookAuthors = asyncHandler(async (req, res) => {
   res.status(201).json({ message: `Set Book ${req.params.id} authors` });
-};
+});
 
 // @desc Update a book
 // @route PUT /api/books/:id
 // @access private
 
-const updateBook = (req, res) => {
+const updateBook = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Book ${req.params.id}` });
-};
+});
 
 // @desc Delete a book
 // @route DELETE /api/books/:id
 // @access private
 
-const deleteBook = (req, res) => {
+const deleteBook = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Book ${req.params.id}` });
-};
+});
 
 // @desc Delete a book author
 // @route DELETE /api/books/:idBook/authors/:idAuthor
 // @access private
 
-const deleteBookAuthor = (req, res) => {
+const deleteBookAuthor = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Delete Author ${req.params.idAuthor} from book ${req.params.id}`,
   });
-};
+});
 
 module.exports = {
   getBooks,
